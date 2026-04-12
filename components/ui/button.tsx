251b8d4e07cea9f10c4cdbede33542c8e699d830
fellis94 <forrestellis94@gmail.com>
2026-04-12 22:15:35 +0000

    Fix radix-ui imports and button component

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -2,7 +2,7 @@
 import * as React from "react";
 import { Slot } from "@radix-ui/react-slot";
 import { cva, type VariantProps } from "class-variance-authority";
-import { cn } from "/components/lib/utils.ts";
+import { cn } from "@/components/lib/utils";
 
 const buttonVariants = cva(
   "inline-flex items-center justify-center whitespace-nowrap rounded-xl text-sm font-medium transition-all active:scale-[0.985] disabled:pointer-events-none disabled:opacity-50",
